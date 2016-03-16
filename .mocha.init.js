e71fcaa9344608e26b8f6b65c56afc104569cf41
Offirmo <offirmo.net@gmail.com>
2016-03-16 14:15:09 +0100

    +

--- .mocha.init.js
+++ .mocha.init.js
@@ -2,19 +2,13 @@
 
 /** Bootstrap mocha/chai unit tests for SERVER
  */
-//import util from 'util';
 
-//import mocha from 'mocha';
-//console.log('mocha', util.inspect(mocha, {colors: true}));
-
-import chai from 'chai';
-import chaiAsPromised from 'chai-as-promised';
-import chaiDatetime from 'chai-datetime';
-import chaiThings from 'chai-things';
-import sinon from 'sinon';
-import sinonChai from 'sinon-chai';
-
-//global.mocha.setup('bdd');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const chaiDatetime = require('chai-datetime');
+const chaiThings = require('chai-things');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
 
 chai.use(chaiAsPromised);
 chai.use(chaiDatetime);
